dc00d75adf05294c1be1976be0bc37db20c9e665
manoj kumar <manojthakur614@gmail.com>
2024-10-13 23:15:25 +0530

    filter tab

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -2,8 +2,9 @@ import logo from './logo.svg';
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
 import './App.css';
-import {fetchTopAlbums, fetchNewAlbums} from './api/api'
+import {fetchTopAlbums, fetchNewAlbums,fetchSongs} from './api/api'
 import Section from './components/Section/Section';
+import FilterSection from './components/FilterSection/FilterSection';
 import { useEffect, useState } from 'react';
 import styles from "./App.module.css"
 
@@ -15,6 +16,12 @@ function App() {
  //states related to the working of filterSection of songs:
  //state to store  original array of songs(unfiltered)
  const[songsData, setSongsData]=useState([]);
+
+ //to store the index selected inn filters, can be ignored
+ const[value,setValue]= useState(0);
+
+ //to store the finally filtered songs
+ const[filteredData, setFilteredData]=useState([]);
 
  const generateTopAlbumSongs=async()=>{
   try{
@@ -40,10 +47,65 @@ const generateNewAlbumSongs=async()=>{
 }
 
 
+const generateSongs=async()=>{
+  try{
+    console.log("generateSongs");
+    const res=await fetchSongs();
+    setSongsData(res);
+    setFilteredData(res);
+  }
+  catch(error){
+    return null;
+  }
+}
+
+//function to generate filtered songs after selecting one tab
+const generateNewSongs=(index)=>{
+
+let key="";
+if(index===0){
+  // suppose someOne select 0th tab after 2nd tab 
+  //set the default songsData as the final filtered data, bcz we need to show all of songs now
+  generateSongs();
+  return;
+}
+else if(index===1){
+  key="rock";
+}
+else if(index===2){
+  key="pop";
+}
+
+else if(index===3){
+  key="jazz";
+}
+else if(index===4){
+  key="blues";
+}
+
+let newSongsArray=songsData.filter((song)=>{
+  console.log("key: ",key)
+  return(song.genre.key===key);
+})
+
+console.log("generateNewSongs triggered and filtered this Data: ", newSongsArray)
+setFilteredData(newSongsArray);
+}
+//rock pop jazz blues
+
+
+//to handle any change in the selected tab of the songs section and call the generateNewSongs
+const handleChangeIndex= async(newValue)=>{
+console.log("handleChangeIndex triggered with newValue: ",newValue)
+setValue(newValue);
+generateNewSongs(newValue);
+}
+
 
 useEffect(()=>{
   generateTopAlbumSongs();
   generateNewAlbumSongs();
+  generateSongs();
  
 },[])
 
@@ -54,6 +116,7 @@ useEffect(()=>{
       <div className={styles.sectionWrapper}>
       <Section type='album' title='Top Albums' data={topAlbumSongs}/>
       <Section type='album' title='New Albums' data={newAlbumSongs}/>
+      <FilterSection  type='song' title='Songs' value={value} filteredData={filteredData} handleChangeIndex={handleChangeIndex}/>
 
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
