35d782ce4aaf25c617955e269b87b078f60904cb
manoj kumar <manojthakur614@gmail.com>
2024-10-13 22:32:50 +0530

    card items

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -2,12 +2,58 @@ import logo from './logo.svg';
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
 import './App.css';
+import {fetchTopAlbums, fetchNewAlbums} from './api/api'
+import Section from './components/Section/Section';
+import { useEffect, useState } from 'react';
+import styles from "./App.module.css"
 
 function App() {
+ //state to store top/new Albums, topAlbumSongs is just an array of 16-17 albums with some info and an array of songs in that album
+ const[topAlbumSongs,setTopAlbumSongs]=useState([]);
+ const[newAlbumSongs,setNewAlbumSongs]=useState([]);
+
+ //states related to the working of filterSection of songs:
+ //state to store  original array of songs(unfiltered)
+ const[songsData, setSongsData]=useState([]);
+
+ const generateTopAlbumSongs=async()=>{
+  try{
+    const res= await fetchTopAlbums();
+  setTopAlbumSongs(res);
+  }
+  catch(error){
+    console.log(error);
+    return null;
+  } 
+}
+
+const generateNewAlbumSongs=async()=>{
+  try{
+    const res= await fetchNewAlbums();
+    console.log(res);
+  setNewAlbumSongs(res);
+  }
+  catch(error){
+    console.log(error);
+    return null;
+  } 
+}
+
+
+
+useEffect(()=>{
+  generateTopAlbumSongs();
+  generateNewAlbumSongs();
+ 
+},[])
+
   return (
     <div className="App">
  <Navbar />
       <Hero />
+      <div className={styles.sectionWrapper}>
+      <Section type='album' title='Top Albums' data={topAlbumSongs}/>
+      <Section type='album' title='New Albums' data={newAlbumSongs}/>
 
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -23,6 +69,8 @@ function App() {
           Learn React
         </a>
       </header> */}
+   
+    </div>
     </div>
   );
 }
