7bb6dc7e3a9dd32dd4c775679a640047e34fabf4
manoj kumar <manojthakur614@gmail.com>
2024-10-13 13:46:31 +0530

    first push

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
+import Navbar from './components/Navbar/Navbar';
+import Hero from './components/Hero/Hero';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+ <Navbar />
+      <Hero />
+
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +22,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
